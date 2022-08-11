d8b56ba38c7136767343e6e97f8516ead8f00eb6
Andrey Stantsov <andrey.stansov@ingka.ikea.com>
2022-08-11 13:48:21 +0300

    Test commit

--- src/Field.java
+++ src/Field.java
@@ -1,4 +1,23 @@
 public class Field {
-    String name;
+    private String fieldName;
+    private String fieldType;
+    private String stringFieldValue;
+    private int intFieldValue;
+    private boolean boolField;
 
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String name) {
+        this.fieldName = name;
+    }
+
+    public String getFieldType(){
+        return fieldType;
+    }
+
+    public void setFieldType(String type) {
+        this.fieldType = type;
+    }
 }
