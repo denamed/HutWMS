ad1701d11599b9155c2bad2bc377675d256b9c2d
Andrey Stantsov <andrey.stansov@ingka.ikea.com>
2022-08-16 15:37:58 +0300

    Test commit

---
+++ src/NumField.java
@@ -0,0 +1,20 @@
+public class NumField {
+    private String fieldName;
+    private int fieldValue;
+
+    public void setFieldName(String name) {
+        this.fieldName = name;
+    }
+
+    public void setFieldValue(int value) {
+        this.fieldValue = value;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public int getFieldValue() {
+        return fieldValue;
+    }
+}
---
+++ src/StringField.java
@@ -0,0 +1,20 @@
+public class StringField {
+    private String fieldName;
+    private String fieldValue;
+
+    public void setFieldName(String name) {
+        this.fieldName = name;
+    }
+
+    public void setFieldValue(String value) {
+        this.fieldValue = value;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getFieldValue() {
+        return fieldValue;
+    }
+}
