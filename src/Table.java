f91909991223ac888f2022758b4d6125573c11d5
Andrey Stantsov <andrey.stansov@ingka.ikea.com>
2022-08-16 15:57:05 +0300

    Test commit

--- src/Table.java
+++ src/Table.java
@@ -1,3 +1,11 @@
 class Table {
     private String tableName;
+
+    public String getFieldName() {
+        return tableName;
+    }
+
+    public void setTableName(String name) {
+        this.tableName = name;
+    }
 }
