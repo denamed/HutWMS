ce5dec2f5b14d6ebfa611730b31b88cdd0f0c739
Andrey Stantsov <andrey.stansov@ingka.ikea.com>
2022-08-24 11:30:45 +0300

    Test commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = null;
+        Statement statement = null;
+//        ResultSet result = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:test.db");
+            statement = connection.createStatement();
+
+            String sql = """
+                    SELECT * FROM Stock
+                    """;
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet result = preparedStatement.executeQuery();
+
+            while (result.next()) {
+                int id = result.getInt("id");
+                String pid = result.getString("palletid");
+                String area = result.getString("area");
+                System.out.println(id + " " + pid + " " + area);
+            }
+            statement.close();
+            connection.close();
+
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
     }
 }
