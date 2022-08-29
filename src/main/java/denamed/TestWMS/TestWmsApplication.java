04b171926522b1299e3734d065cfb9940764a0d7
Andrey Stantsov <andrey.stansov@ingka.ikea.com>
2022-08-29 18:06:42 +0300

    ReNew commit

---
+++ src/main/java/denamed/TestWMS/TestWmsApplication.java
@@ -0,0 +1,13 @@
+package denamed.TestWMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWmsApplication.class, args);
+	}
+
+}
