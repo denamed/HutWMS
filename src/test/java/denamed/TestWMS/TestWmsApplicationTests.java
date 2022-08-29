8b1a30e16f2de2d2590e76bfd363c71a02359c86
Andrey Stantsov <andrey.stansov@ingka.ikea.com>
2022-08-29 18:09:23 +0300

    ReNew commit

---
+++ src/test/java/denamed/TestWMS/TestWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package denamed.TestWMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
