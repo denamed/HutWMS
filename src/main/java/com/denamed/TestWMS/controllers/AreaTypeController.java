2aa17f961dd0ede7e18f081952fb9a3f2953d059
Alexey Bublik <alexey.bublik@ingka.ikea.com>
2022-09-28 16:48:02 +0300

    adding areaType

--- src/main/java/com/denamed/TestWMS/controllers/AreaTypeController.java
+++ src/main/java/com/denamed/TestWMS/controllers/AreaTypeController.java
@@ -29,14 +29,69 @@ public class AreaTypeController {
         return "areaType-list";
     }
 
-    @PostMapping("/areaType")
-    public String addAreaType(@RequestParam Integer areaTypeId,
-                              @RequestParam String areaTypeDesc,
-                              Map<String, Object> model){
+    // Get create AreaType
+    @GetMapping("/areaType-create")
+    public String areaTypeCreate(){
+        return "areaType-create";
+    }
+
+    // Get edit AreaType
+    @GetMapping("/areaType-edit")
+    public String areaTypeEdit(@RequestParam Integer areaTypeId, Model model){
+       try {
+           AreaType areaType = areaTypeService.findById(areaTypeId).get();
+           model.addAttribute("areaType", areaType);
+           return "areaType-edit";
+       }catch(Exception e){
+           model.addAttribute("message", e.getMessage());
+           model.addAttribute("areaType", areaTypeService.findAll());
+           return "areaType-list";
+       }
+    }
+
+    //Post create AreaType
+    @PostMapping("/areaType-create")
+    public String areaTypeCreatePM (@RequestParam Integer areaTypeId,
+                                    @RequestParam String areaTypeDesc,
+                                    Model model){
         AreaType areaType = new AreaType(areaTypeId, areaTypeDesc);
-        areaTypeService.saveAreaType(areaType);
+        try{
+            areaTypeService.areaTypeCreate(areaType);
+            List <AreaType> areaTypes = areaTypeService.findAll();
+            model.addAttribute("areaTypes", areaTypes);
+            model.addAttribute("message", "Area type successfully created.");
+            return "areaType-list";
+        } catch (Exception e){
+            model.addAttribute("message", e.getMessage());
+            return "areaType-create";
+        }
+    }
+
+    //Post edit AreaType
+    @PostMapping("/areaType-edit")
+    public String areaTypeEditPM (@RequestParam Integer areaTypeId,
+                                  @RequestParam String areaTypeDesc,
+                                  Model model){
+        AreaType areaType = new AreaType(areaTypeId, areaTypeDesc);
+        areaTypeService.areaTypeEdit(areaType);
         List<AreaType> areaTypes = areaTypeService.findAll();
-        model.put("areaTypes", areaTypes);
+        model.addAttribute("areaTypes",areaTypes);
+        model.addAttribute("message", "Area type successfully edited.");
+        return "areaType-list";
+    }
+
+    //Post delete AreaType
+    @PostMapping("/areaType-delete")
+    public String areaTypeDeletePM (@RequestParam Integer areaTypeId,
+                                    Model model){
+        try {
+            areaTypeService.deleteAreaType(areaTypeId);
+            model.addAttribute("message", "Area type successfully deleted.");
+        }catch(Exception e){
+            model.addAttribute("message", "Unable to delete!");
+        }
+        List<AreaType> areaTypes = areaTypeService.findAll();
+        model.addAttribute("areaTypes", areaTypes);
         return "areaType-list";
     }
 
--- src/main/java/com/denamed/TestWMS/services/AreaTypeService.java
+++ src/main/java/com/denamed/TestWMS/services/AreaTypeService.java
@@ -5,6 +5,8 @@ import com.denamed.TestWMS.repositories.AreaTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class AreaTypeService {
@@ -15,8 +17,29 @@ public class AreaTypeService {
     }
 
     public List<AreaType> findAll(){ return areaTypeRepository.findAll();}
-    
 
+    public Optional<AreaType> findById(int areaTypeID) throws Exception{
+        Optional<AreaType> areaType = areaTypeRepository.findById(areaTypeID);
+        if(areaType.isEmpty()){
+            throw new Exception("<div class=\"alert\">Required area type not exists!</div>");
+        }
+        else{
+            return areaType;
+        }
+    }
+
+    public void areaTypeCreate(AreaType areaType) throws Exception{
+        if(areaTypeRepository.existsById(areaType.getAreaTypeId())){
+            throw new Exception("<div class=\"alert\">Required area type not exists!</div>");
+        }
+        else{
+            areaTypeRepository.save(areaType);
+        }
+    }
+
+    public void areaTypeEdit (AreaType areaType){
+        areaTypeRepository.save(areaType);
+    }
 
     public AreaType saveAreaType(AreaType areaType) {
 
